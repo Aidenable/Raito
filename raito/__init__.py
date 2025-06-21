fe5a9a27df497b783a5edf3147a5cbd27c717abc
Aiden <155806565+Aiden-The-Dev@users.noreply.github.com>
2025-06-21 04:50:22 +0300

    fix: change namespace to class attrs

--- raito/__init__.py
+++ raito/__init__.py
@@ -1,15 +1,14 @@
-from types import SimpleNamespace
-
 from raito.utils.configuration import Configuration
 
 from .core.raito import Raito
 from .plugins.roles import Role, roles
 
-rt = SimpleNamespace(
-    Raito=Raito,
-    roles=roles,
-    Role=Role,
-    Configuration=Configuration,
-)
+
+class rt:
+    Raito = Raito
+    roles = roles
+    Role = Role
+    Configuration = Configuration
+
 
 __all__ = ("Raito", "rt")
