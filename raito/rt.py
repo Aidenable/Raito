5789ce5380b605aa8cb1db30a4696d4a0eda0ab5
Aiden <155806565+Aiden-The-Dev@users.noreply.github.com>
2025-06-28 01:25:56 +0300

    feat(keyboards): add `keyboard` shortcut to `rt`

--- raito/rt.py
+++ raito/rt.py
@@ -1,6 +1,7 @@
 from .core.raito import Raito
 from .plugins.pagination import on_pagination
 from .plugins.roles import Role, roles
+from .utils.keyboard import keyboard
 from .utils.loggers import log
 
 debug = log.debug
@@ -9,6 +10,7 @@ __all__ = (
     "Raito",
     "Role",
     "debug",
+    "keyboard",
     "log",
     "on_pagination",
     "roles",
