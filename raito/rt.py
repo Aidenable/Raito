cdbb937c9152ba144d25df327ef802380eb072b0
Aiden <155806565+Aiden-The-Dev@users.noreply.github.com>
2025-06-28 18:02:42 +0300

    refactor: move keyboards from 'utils/' to 'plugins/'

--- raito/rt.py
+++ raito/rt.py
@@ -1,7 +1,7 @@
 from .core.raito import Raito
+from .plugins import keyboards as keyboard
 from .plugins.pagination import on_pagination
 from .plugins.roles import Role, roles
-from .utils import keyboards as keyboard
 from .utils.loggers import log
 
 debug = log.debug
