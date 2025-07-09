a866d076d6ca1e5d95546c2edd73f9eeb74a6881
Aiden <155806565+Aiden-The-Dev@users.noreply.github.com>
2025-07-09 17:05:51 +0300

    refactor(commands): make internal functions private and add `__init__.py`

--- raito/plugins/commands/flags.py
+++ raito/plugins/commands/flags.py
@@ -1,21 +1,22 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from collections.abc import Callable
+from typing import TYPE_CHECKING, Any
 
 from aiogram.dispatcher.flags import Flag, FlagDecorator
 
 if TYPE_CHECKING:
     from aiogram.utils.i18n.lazy_proxy import LazyProxy  # type: ignore
 
-__all__ = ("description",)
+__all__ = ("description", "hidden", "params")
 
 
 def description(description: str | LazyProxy) -> FlagDecorator:
     return FlagDecorator(Flag("raito__description", value=True))(description)
 
 
-def hidden() -> FlagDecorator:
-    return FlagDecorator(Flag("raito__hidden", value=True))
+def hidden(func: Callable) -> Callable[..., Any]:
+    return FlagDecorator(Flag("raito__hidden", value=True))(func)
 
 
 def params(**kwargs: type[str] | type[int] | type[bool] | type[float]) -> FlagDecorator:
