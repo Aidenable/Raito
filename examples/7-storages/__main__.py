bf34460d14e7f45a8811ed6f3ea2148303a101b2
Aiden <155806565+Aiden-The-Dev@users.noreply.github.com>
2025-07-13 15:47:08 +0300

    examples: add storages

---
+++ examples/7-storages/__main__.py
@@ -0,0 +1,39 @@
+import asyncio
+from pathlib import Path
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+
+from raito.core.raito import Raito
+from raito.utils.storages.json import JSONStorage
+from raito.utils.storages.sql import get_sqlite_storage
+
+TOKEN = "TOKEN"
+DEBUG = False
+DEVELOPER = 1234
+ROOT_DIR = Path(__file__).parent
+
+
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
+dispatcher = Dispatcher(storage=JSONStorage(ROOT_DIR / "fsm.json"))
+
+SQLiteStorage = get_sqlite_storage()
+sqlite_url = f"sqlite+aiosqlite:///{(ROOT_DIR / 'raito.db').as_posix()}"
+
+raito = Raito(
+    dispatcher,
+    ROOT_DIR / "handlers",
+    developers=[DEVELOPER],
+    production=not DEBUG,
+    storage=SQLiteStorage(sqlite_url),
+)
+raito.init_logging()
+
+
+async def main() -> None:
+    await raito.setup()
+    await dispatcher.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
