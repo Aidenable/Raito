c3d4691566af7fbcc5c4d724be8f2ba96bb26ac4
Aiden <155806565+Aiden-The-Dev@users.noreply.github.com>
2025-07-13 15:49:30 +0300

    examples: add pagination

---
+++ examples/9-pagination/__main__.py
@@ -0,0 +1,42 @@
+import asyncio
+from pathlib import Path
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+
+from raito.core.raito import Raito
+from raito.utils.configuration import (
+    PaginationControls,
+    PaginationStyle,
+    PaginationTextFormat,
+    RaitoConfiguration,
+)
+
+TOKEN = "TOKEN"
+HANDLERS_DIR = Path(__file__).parent / "handlers"
+DEBUG = False
+
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
+dispatcher = Dispatcher()
+raito = Raito(
+    dispatcher,
+    HANDLERS_DIR,
+    developers=[],
+    production=not DEBUG,
+    configuration=RaitoConfiguration(
+        pagination_style=PaginationStyle(
+            controls=PaginationControls(previous="<", next=">"),
+            text_format=PaginationTextFormat(counter_template="Page {current} of {total}"),
+        )
+    ),
+)
+raito.init_logging()
+
+
+async def main() -> None:
+    await raito.setup()
+    await dispatcher.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
