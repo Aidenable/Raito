8c49bead68bbb572363e771f22f680f858219a27
Aiden <155806565+Aiden-The-Dev@users.noreply.github.com>
2025-07-13 15:45:38 +0300

    examples: add base-bot

---
+++ examples/1-base-bot/__main__.py
@@ -0,0 +1,24 @@
+import asyncio
+from pathlib import Path
+
+from aiogram import Bot, Dispatcher
+
+from raito.core.raito import Raito
+
+TOKEN = "TOKEN"
+HANDLERS_DIR = Path(__file__).parent / "handlers"
+DEBUG = False
+
+bot = Bot(token=TOKEN)
+dispatcher = Dispatcher()
+raito = Raito(dispatcher, HANDLERS_DIR, developers=[], production=not DEBUG)
+raito.init_logging()
+
+
+async def main() -> None:
+    await raito.setup()
+    await dispatcher.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
