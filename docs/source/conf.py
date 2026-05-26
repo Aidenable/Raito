e5e88a7fcae5cdf9a412fc7902dea12bd63bf005
lantrik <vitali.amoyan@icloud.com>
2026-05-26 19:28:29 +0700

    style: set version to 1.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "Raito"
 copyright = "2025, Aiden"
 author = "Aiden"
-release = "1.3.6"
+release = "1.4.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
